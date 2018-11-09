a05513cefe60f284241482b6567d34daca84c341
Shahriar Saif <shahriar489@gmail.com>
2018-11-09 23:06:43 +0600

    Add files via upload

--- perceptron.py
+++ perceptron.py
@@ -1,84 +1,62 @@
-import random as rnd
-#perceptron to learn to classify
-#even and odd binary bit patterns
-#even = 0, odd = 1
+import numpy as np
+import math
+from sklearn.datasets import load_iris
 
-def dec_to_bin(a, n):
-    pat = []
-    invrtd = []
-    while a != 0:
-        r = a % 2
-        a = int(a / 2)
-        pat.append(r)
-    i = len(pat)-1
-    while i >= 0:
-        invrtd.append(pat[i])
-        i -= 1
-    if len(invrtd) < n:
-        m = n - len(invrtd)
-        for i in range(m):
-            invrtd.insert(0, 0)
-    invrtd.append(1)
-    return invrtd
+alpha = 0.3
+portion = 20
 
-def vec_add(a, b):
-    r = []
-    n = len(a)
-    for i in range(n):
-        r.append(a[i] + b[i])
-    return r
-    
-def vec_sc_mul(a, k):
-    r = [] 
-    n = len(a)
-    for i in range(n):
-        r.append(a[i] * k)
-    return r
+def prepare_data():
+	tr_x = []
+	tr_y = []
+	test_x = []
+	test_y = []
+	data = load_iris()
+	m = len(data)
+	indx = np.random.permutation(m)
+	tr_x = data.data[indx[:portion]]
+	tr_y = data.target[indx[:portion]]
+	test_x = data.data[indx[portion:]]
+	test_y = dat.target[indx[portion:]]
+	tr_data = []
+	test_data = []
+	
+	m = len(tr_x)
+	for i in range(m):
+		tr_x[i].append(1)
+		if tr_y[i] == 2:
+			tr_y[i] = 1
+		tr_data.append((tr_x[i], tr_y[i]))
+	m = len(test_x)
+	for i in range(m):
+		test_x.append(1)
+		if test_y[i] == 2:
+			test_y[i] = 1
+		test_data.append((test_x[i], test_y[i]))
+	return (tr_data, test_data)
 
-def vec_dot(a, b):
-    sum = 0
-    n = len(a)
-    for i in range(n):
-        sum += a[i] * b[i]
-    return sum
+def perceptron(w, x):
+	if np.dot(w, x) >= 0:
+		return 1
+	else:
+		return 0
 
-def perceptron(x, w):
-    a = vec_dot(x, w)
-    if a >= 0:
-        return 1
-    return 0
+def train_perceptron(data):
+	m = len(data)
+	w = np.random.random(len(data[0][0]))
+	for i in range(m):
+		delta = data[i][1] - perceptron(w, data[i][0])
+		w = w + alpha * (delta * data[i][0])
+	return w
 
-def train(tr, w, alpha):
-    m = len(tr)
-    for i in range(m):
-        delta = tr[i][1] - perceptron(tr[i][0], w)
-        w = vec_add(w, vec_sc_mul(vec_sc_mul(tr[i][0], delta), alpha))
-    return w
-
-def define_pattern(nf, m):
-    pat = []
-    for i in range(m):
-        x = dec_to_bin(i, nf)
-        y = 0
-        if i % 2 == 0:
-            y = 0
-        else:
-            y = 1
-        pat.append((x, y))
-    return pat
-
-nf = int(input('enter the number of features(#binary bits to be used): ')) 
-m = int((2 ** nf) / 2)
-tr = define_pattern(nf, m)
-w = []
-for i in range(nf + 1):
-    w.append(rnd.random())
-w = train(tr, w, 0.3)
-x = int(input('enter a number: '))
-x = dec_to_bin(x, nf)
-y = perceptron(x,w)
-if y == 1:
-    print('the number is: odd')
-else:
-    print('the number is: even')
-print(w)
+def main():
+	(tr_data, test_data) = prepare_data()
+	w = train_perceptron(tr_data)
+	mc = 0
+	m = len(test_data)
+	for i in range(m):
+		resp = perceptron(w, test_data[i][0])
+		if resp != test_data[i][1]:
+			mc += 1
+	print('number of misclassification = ', mc)
+	
+main() 
